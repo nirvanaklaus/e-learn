e58df74fd834ef8642eafd0ef8e75c6e73a61eb7
Lumi001 <pelumi.akinola@stu.cu.edu.ng>
2020-09-23 16:19:31 +0100

    sixth

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './pages/signIn/signIn';
+import SignUp from './pages/signup/signUp';
 
 function App() {
   return (
     <div className="App">
-      <SignIn/>
+      {/* <SignIn/> */}
+      <SignUp/>
     </div>
   );
 }
