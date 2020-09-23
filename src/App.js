9b48077f0cc54e83f9559f7fab3c024392d95059
Nicholas Okonneh <nirvanaklaus@gmail.com>
2020-09-23 18:17:54 +0100

    Sixth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SignIn from './pages/signIn/signIn';
 import Dashboard from './pages/dashboard/dashboard';
@@ -7,7 +6,7 @@ import Dashboard from './pages/dashboard/dashboard';
 function App() {
   return (
     <div className="App">
-      {/* <SignIn/> */}
+      <SignIn/>
       <Dashboard/>
     </div>
   );
